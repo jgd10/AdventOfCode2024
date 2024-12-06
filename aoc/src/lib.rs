dfc53a486710d60eddbe10c212fac9f5b9f5a6c4
James Derrick <jamesgordonderrick@gmail.com>
2024-12-06 13:27:57 +0000

    removed unused aoc functions

--- aoc/src/lib.rs
+++ aoc/src/lib.rs
@@ -21,22 +21,6 @@ pub fn write_vector_to_text_file<T: std::fmt::Debug>(my_vector: Vec<T>){
     file.flush().unwrap();
 }
 
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
-
 #[derive(Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord, Hash)]
 #[allow(dead_code)]
 pub enum InputType {
