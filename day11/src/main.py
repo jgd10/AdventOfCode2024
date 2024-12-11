0b79ab00e1537e97b261d2b35ac64ae692a613f0
James Derrick <jamesgordonderrick@gmail.com>
2024-12-11 13:08:43 +0000

    day 11 complete and both parts solved in same func, although part 1 preserved in file too

--- day11/src/main.py
+++ day11/src/main.py
@@ -1,4 +1,4 @@
-from pycparser.ply.yacc import resultlimit
+from typing import Tuple
 
 
 def scalar_digit_multiply(number: list[int], digit: int):
@@ -17,7 +17,7 @@ def scalar_digit_multiply(number: list[int], digit: int):
     return result
 
 
-def long_sum(data: list[list[int]]):
+def long_sum(data: list[list[int]]) -> tuple[int]:
     carry = 0
     result = []
     while any([len(row) > 0 for row in data]):
@@ -31,10 +31,10 @@ def long_sum(data: list[list[int]]):
         result.insert(0, int(ans))
     if carry > 0:
         result.insert(0, carry)
-    return result
+    return tuple(result)
 
 
-def long_multiply(num1: list[int], num2: list[int]):
+def long_multiply(num1: tuple[int, ...], num2: tuple[int, ...]) -> tuple[int]:
     """Long multiplication.
 
     Equivalent to,
@@ -58,7 +58,7 @@ def long_multiply(num1: list[int], num2: list[int]):
     return long_sum(results)
 
 
-def strip_leading_zeroes(number: list[int]):
+def strip_leading_zeroes(number: tuple[int, ...]) -> tuple[int, ...]:
     new_num = []
     first_digit_reached = False
     for n in number:
@@ -69,7 +69,7 @@ def strip_leading_zeroes(number: list[int]):
             first_digit_reached = True
     if len(new_num) == 0:
         new_num = [0]
-    return new_num
+    return tuple(new_num)
 
 
 def part1():
@@ -85,11 +85,10 @@ def part1():
         data = data[0]
         nums = data.split(' ')
     numbers = [[int(c) for c in number] for number in nums]
-    #numbers = [[1, 2, 5], [1, 7]]
     print(numbers)
-    blinks = 75
+    blinks = 25
     for i in range(blinks):
-        print(i)
+        print(i, len(numbers))
         new_numbers = []
         for number in numbers:
             if len(number) == 1 and number[0] == 0:
@@ -107,20 +106,54 @@ def part1():
     print(f'Part 1: {len(numbers)}')
 
 
-def test():
-    print(long_multiply([2, 0, 2, 4], [7]))
-    print(long_multiply([7], [2, 0, 2, 4]))
-    print(long_multiply([2], [9, 9, 9, 9]))
-    print(long_multiply([7,7,0,8], [2, 0, 2, 4]))
-    print(strip_leading_zeroes([0,0,4]))
-    print(strip_leading_zeroes([0, 0, 0]))
-    print(strip_leading_zeroes([0, 0, 4, 1]))
-    print(strip_leading_zeroes([0, 0, 4, 0, 0]))
+def find_number_of_stones_after(blinks: int) -> int:
+    with open('../input.txt') as f:
+        data = [s.strip('\n') for s in f.readlines()]
+        data = data[0]
+        nums = data.split(' ')
+    numbers = {tuple(int(c) for c in number) for number in nums}
+    old_number_counts = {n: 1 for n in numbers}
+    for i in range(blinks):
+        new_counts = {}
+        for number in old_number_counts:
+            if len(number) == 1 and number[0] == 0:
+                if (1,) not in new_counts:
+                    new_counts[(1,)] = 0
+                new_counts[(1,)] += old_number_counts[(0,)]
+                continue
+            if (length := len(number)) % 2 == 0:
+                new_length = length // 2
+                new_num1 = number[:new_length]
+                new_num2 = strip_leading_zeroes(number[new_length:])
+                if new_num1 not in new_counts:
+                    new_counts[new_num1] = 0
+                if new_num2 not in new_counts:
+                    new_counts[new_num2] = 0
+                new_counts[new_num1] += old_number_counts[number]
+                new_counts[new_num2] += old_number_counts[number]
+                continue
+            new_num = long_multiply(number, (2, 0, 2, 4))
+            if new_num not in new_counts:
+                new_counts[new_num] = 0
+            new_counts[new_num] += old_number_counts[number]
+        old_number_counts = new_counts.copy()
+    return sum([v for v in old_number_counts.values()])
 
+
+def test():
+    print(long_multiply((2, 0, 2, 4), (7,)))
+    print(long_multiply((7,), (2, 0, 2, 4)))
+    print(long_multiply((2,), (9, 9, 9, 9)))
+    print(long_multiply((7,7,0,8), (2, 0, 2, 4)))
+    print(strip_leading_zeroes((0,0,4)))
+    print(strip_leading_zeroes((0, 0, 0)))
+    print(strip_leading_zeroes((0, 0, 4, 1)))
+    print(strip_leading_zeroes((0, 0, 4, 0, 0)))
 
 
 def main():
-    part1()
+    print(f'Part 1: {find_number_of_stones_after(25)}')
+    print(f'Part 2: {find_number_of_stones_after(75)}')
 
 
 if __name__ == '__main__':
